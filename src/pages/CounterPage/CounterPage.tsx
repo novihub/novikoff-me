2829699449fa1a5d73cbf4170f32b0057ee0e185
novikoff <novikoff04@gmail.com>
2024-09-18 22:36:58 +0500

    Initialized

---
+++ src/pages/CounterPage/CounterPage.tsx
@@ -0,0 +1,12 @@
+import { Counter } from '../../components/Counter'
+
+const CounterPage = () => {
+	return (
+		<div>
+			<h1>Counter Page</h1>
+			<Counter />
+		</div>
+	)
+}
+
+export default CounterPage
