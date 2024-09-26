fc64b49f0439b44a7be8cfbffbf835af3b15f58d
Max Novikoff <novikoff-mac@Maxs-MacBook-Air.local>
2024-09-26 23:43:37 +0500

    Refactored

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -15,6 +15,8 @@ export function useTheme(): UseThemeResult {
 		localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
 	}
 
+	setTheme(Theme.DARK) // HARD CODED !!!
+
 	return {
 		toggleTheme,
 		theme
