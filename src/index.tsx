f996882220e48f9e08f0cafa52200dc8906ca0e3
novikoff0 <novikoff04@gmail.com>
2024-10-06 15:33:31 +0500

    hash router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import StoreProvider from 'app/providers/StoreProvider/ui/StoreProvider'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { App } from './app/App'
 import ThemeProvider from './app/providers/ThemeProvider'
 
@@ -8,11 +8,11 @@ const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
-	<BrowserRouter basename='/'>
+	<HashRouter>
 		<StoreProvider>
 			<ThemeProvider>
 				<App />
 			</ThemeProvider>
 		</StoreProvider>
-	</BrowserRouter>
+	</HashRouter>
 )
