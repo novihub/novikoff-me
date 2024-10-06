11fba68e4be97341c95ce195bdb346e5f638e650
novikoff0 <novikoff04@gmail.com>
2024-10-06 15:14:29 +0500

    fixing issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import StoreProvider from 'app/providers/StoreProvider/ui/StoreProvider'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { App } from './app/App'
 import ThemeProvider from './app/providers/ThemeProvider'
 
@@ -8,11 +8,11 @@ const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
-	<HashRouter>
+	<BrowserRouter basename='/'>
 		<StoreProvider>
 			<ThemeProvider>
 				<App />
 			</ThemeProvider>
 		</StoreProvider>
-	</HashRouter>
+	</BrowserRouter>
 )
