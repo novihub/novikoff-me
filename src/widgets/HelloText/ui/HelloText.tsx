57b096e4472d6761cc612e9052e7a40b42f254ac
novikoff0 <novikoff04@gmail.com>
2024-10-19 21:31:12 +0500

    added typing effect

--- src/widgets/HelloText/ui/HelloText.tsx
+++ src/widgets/HelloText/ui/HelloText.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './HelloText.module.scss'
 
@@ -7,12 +7,35 @@ interface HelloTextProps {
 }
 
 export const HelloText: FC<HelloTextProps> = ({ className }) => {
+	const typedTextRef = React.useRef(null)
+	const [isTypingComplete, setIsTypingComplete] = React.useState(false)
+	const text = 'Frontend Developer'
+	let index = 0
+
+	const typeText = () => {
+		if (typedTextRef) {
+			typedTextRef.current.innerHTML += text.charAt(index)
+			index++
+
+			if (index < text.length) setTimeout(typeText, 100)
+			else setIsTypingComplete(true)
+		}
+	}
+
+	useEffect(() => {
+		if (typedTextRef.current) {
+			typedTextRef.current.innerHTML = '> '
+		}
+		setIsTypingComplete(false)
+		typeText()
+	}, [])
+
 	return (
 		<section className={classNames(cls.helloText, {}, [className])}>
 			<div className={cls.topText}>
 				<h5>Hi all. I am</h5>
 				<h1>Novikoff Max</h1>
-				<h2>{'>'} Frontend Developer</h2>
+				<h2 className={classNames(cls.typing, {[cls.cursorBlink]: isTypingComplete}, [])} ref={typedTextRef}></h2>
 			</div>
 			<div className={cls.bottomText}>
 				<p className={cls.commentJs}>// complete the game to continue</p>
